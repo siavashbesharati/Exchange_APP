71f7eb0d51f00107197c1fb778e7e9d5cfcae368
siavash <siavashbesharati@gmail.com>
2026-07-14 16:38:10 +0400

    update action for test

---
+++ ForexExchange/deploy.ps1
@@ -0,0 +1,12 @@
+# ===== Configuration =====
+$Project = "F:\Expedia\Exsora\Exchange_APP\Exchange_APP.csproj"
+$PublishFolder = "F:\Expedia\Exsora\Exchange_APP\publish"
+
+$FtpHost = "pnl.taban-group.com"
+$Username = "sia"
+$Password = "roberto2026@#$ASD"
+
+# Remote IIS folder
+$RemoteFolder = "/httpdocs/"
+
+$WinSCP = "C:\Program Files (x86)\WinSCP\WinSCP.com"
